1a4bebd3f28369ce263a4dad5decaa3567a976ed
Shanugarg499 <shanugarg499@gmail.com>
2021-09-25 13:01:11 +0530

    Login page interface.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,13 +2,13 @@ import './Home.css';
 import {Carousel} from '3d-react-carousal';
 
 import slider from './slider.js';
-import Card from 'C:/Users/pc/Documents/Desktop folder/imp/lala-air-solution/src/Card/Card.js';
+import Card from '../Card/Card.js';
 
 const Home = () => {
     let slides = [
-    <img src= "https://source.unsplash.com/1600x900/?aeroplane,airport" alt="1" />,
-        <img src="https://source.unsplash.com/1600x900/?sky,parachute" alt="2" />  ,
-        <img src="https://source.unsplash.com/1600x900/?waterfall,water" alt="3" />  ,
+        <img src= "https://source.unsplash.com/1600x900/?trekking,mountain" alt="1" />,
+        <img src="https://source.unsplash.com/1600x900/?airport,aeroplane" alt="2" />  ,
+        <img src="https://source.unsplash.com/1600x900/?waterfall,water,mountain" alt="3" />,
         <img src="https://source.unsplash.com/1600x900/?nature,sky" alt="4" />   ];
         return (
             <Carousel slides={slides} />
