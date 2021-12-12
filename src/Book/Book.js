0ea52d9b1eeb6f5c093b0d3d7fe8f704e474eb3f
Shanugarg499 <shanugarg499@gmail.com>
2021-12-12 17:41:17 +0530

    pseudo create account + almost perfect book flight

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -6,17 +6,20 @@ import 'firebase/firestore';
 
 const bookseat = (passenger_details, transaction_details) => {
     
-    const db = firebase.firestore();
-    // add transaction to user_history
     try{
+    
+      const db = firebase.firestore();  
       db.collection('payment_history').doc(passenger_details.user_id).set({[transaction_details.transaction_id]: transaction_details});
-    // update seat_map
-    for(var seatno in passenger_details.seats) {
-      db.collection([passenger_details.seats.seatno.airways])
-      .doc([passenger_details.seats.seatno.flight_date + passenger_details.seats.seatno.flight_id + passenger_details.seats.seatno.type])
-      .set({[passenger_details.seats.seatno.seat_num]: passenger_details.user_id})
-    }
-    } catch(e) {
+      
+      const data = {}
+      for(var i=0; i<passenger_details.seats.length; i++)
+        data[passenger_details.seats[i].seat_num] = passenger_details.user_id
+      db.collection(passenger_details.airways)
+        .doc(passenger_details.flight_date + passenger_details.flight_id)
+        .set(data)
+    
+      } catch(e) {
+      
       console.log("Hey there! There's error: "+e )
     } 
   }
@@ -26,42 +29,32 @@ const Book = () => {
   const passenger = {
     name: 'Shanu Garg',
     user_id: 'Shanu_garg11112021184529',
-    total_seats: '4',
-    seats: {
-      1: {
-        airways: 'Jet',
-        flight_id: 'random_flight_id',
-        flight_date: '21122021',
+    airways: 'Jet',
+    flight_date: '21122021',
+    flight_id: 'random_flight_id',
+    seats: [
+      {
         type: 'B',
         seat_num: '47a'
       },
-      2: {
-        airways: 'Jet',
-        flight_id: 'random_flight_id',
-        flight_date: '21122021',
+      {
         type: 'B',
         seat_num: '47b'
       },
-      3: {
-        airways: 'Jet',
-        flight_id: 'random_flight_id',
-        flight_date: '21122021',
+      {
         type: 'B',
         seat_num: '47c'
       },
-      4: {
-        airways: 'Jet',
-        flight_id: 'random_flight_id',
-        flight_date: '21122021',
+      {
         type: 'B',
         seat_num: '46a'
       } 
-    }
+    ]
   }
   const transaction = {
     user_id: 'Shanu_garg11112021184529',
     amount: '100000',
-    transaction_id: '123456789',
+    transaction_id: 'transaction_id',
     method: 'credit card',
     date: '11112021',
     time: '181523'
@@ -71,7 +64,7 @@ const Book = () => {
     <img className = 'Bookimg' src='https://static.goindigo.in/content/dam/indigov2/6e-website/aboutus/flight-booking/TicketBooking-Page-Banner.jpg' alt='image loading...'></img>
 
     <rb.Button variant="primary" type="submit" className='submitbutton' onClick={() => bookseat(passenger, transaction)}>
-        Test-ping-to-server
+        Book a pseudo flight to server
     </rb.Button>
     <Footer />
     </>
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,34 +1,33 @@
 import * as rb from 'react-bootstrap';
 import './Login.css';
-import { Footer } from '../Footer/Footer';
-import firebase from '../utils/firebase';
 import 'firebase/firestore';
 
 // signup method
 
-const adduser = (name, email, phone_num, password) => {
+// const adduser = (name, email, phone_num, password) => {
     
-    const date = new Date();
-    const user_id = 'Shanu_garg'+date.getDate()+date.getMonth()+date.getFullYear()+date.getHours()+date.getMinutes()+date.getSeconds();
-    const user_data = { name, email, phone_num, password  }
-    const map_data = { [email] : user_id }
-    const data_map = { [user_id] : email }
+//     const date = new Date();
+//     const user_id = 'Shanu_garg'+date.getDate()+date.getMonth()+date.getFullYear()+date.getHours()+date.getMinutes()+date.getSeconds();
+//     const user_data = { name, email, phone_num, password  }
+//     const map_data = { [email] : user_id }
+//     const data_map = { [user_id] : email }
 
   
-    console.log('function called')
-    const db = firebase.firestore();
-    // write code to check if current email id is already registered or not
-    try{
-    
-        db.collection('maps').doc('user_map').set(map_data);
-        console.log('done here')
-        db.collection('users').doc(user_id).set(user_data);
-    
-        db.collection('maps').doc('user_map').set(data_map);
-    }catch(e) {
-        console.log('Error: '+e)
-    }
-  }
+//     console.log('function called')
+//     const db = firebase.firestore();
+//     try{
+
+//         console.log(map_data)
+//         db.collection('maps').doc('user_map').set(map_data);
+//         console.log('done map_data')
+//         db.collection('users').doc(user_id).set(user_data);
+//         console.log('done user_data')
+//         db.collection('maps').doc('user_map').set(data_map);
+//         console.log('done data_map')
+//     }catch(e) {
+//         console.log('Error: '+e)
+//     }
+//   }
 
 const Login = () => {
     return (
@@ -56,12 +55,12 @@ const Login = () => {
             <br />
             <br />
             <br />
-            <rb.Button variant="primary" type="submit" className='submitbutton' onClick={() => adduser('Shanu Garg', '181220050@nitdelhi.ac.in', '9876543210', '9876543210')}>
+            {/* <rb.Button variant="primary" type="submit" className='submitbutton' onClick={() => adduser('Shanu Garg', '181220050@nitdelhi.ac.in', '9876543210', '9876543210')}>
                 Create test account
-            </rb.Button>
+            </rb.Button> */}
             </rb.Form>
             </div>
-            <Footer className='footer'/>
+            {/* <Footer className='footer'/> */}
         </>
     );
 };
