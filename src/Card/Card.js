ec742439581375e9c659629c5a82531b69f45b8d
NITESHGUPTAKTL9 <72707575+NITESHGUPTAKTL9@users.noreply.github.com>
2021-12-20 16:50:26 +0530

    props in card

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,22 +3,24 @@ import '../Card/Card.css';
 import { Footer } from '../Footer/Footer';
 export default function Card(props) {
   console.log(props);
+  let color=props.product.color;
+  console.log(color);
   return (
 <>
 
     <div className="shift">
-      <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
 
 
             <img className="card-image" src={props.product.img} alt="Logo" />
 
           </div>
-          <div class="flip-card-back">
-            <h1>HDFC BANK</h1>
-            <p>Credit / Debit  Card</p>
-            <p>Valid Today</p>
+          <div  style={{backgroundColor:color ,color:'white'}} className="flip-card-back">
+            <h2 style={{color:'orange'}}>{props.product.title}</h2>
+            <h5>{props.product.offer}</h5>
+            <h6 style={{color:'yellow'}}>{props.product.validity}</h6>
           </div>
         </div>
       </div>
