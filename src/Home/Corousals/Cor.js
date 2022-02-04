1cded89bd79141c89e419a3c918b7409c5b60bbb
Nitesh Gupta <niteshguptaktl9@gmail.com>
2022-02-04 18:28:57 +0530

    gradient

--- src/Home/Corousals/Cor.js
+++ src/Home/Corousals/Cor.js
@@ -3,11 +3,12 @@ import "./cor.css";
 const Cor = () => {
     return (
         <div className="container">
-            <img src="https://images.pexels.com/photos/358220/pexels-photo-358220.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" className="container2" alt="Notebook"/>
+       
+            {/* <img src="https://images.pexels.com/photos/358220/pexels-photo-358220.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" className="container2" alt="Notebook"/> */}
             <div className="content">
-                <h1>Heading</h1>
-                <p>Lorem ipsum..</p>
-                <button>click here</button>
+                <h3>LALA AIRLINES SERVING FOR YEARS</h3>
+                <h4>Providing Services To Travelers At Affordable Prices</h4>
+                <button className="btn warning">Presentation</button>
             </div>
         </div>
     )
