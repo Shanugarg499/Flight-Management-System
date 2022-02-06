940624970cdcc6967a04c82971157f906ad8c93e
Nitesh Gupta <niteshguptaktl9@gmail.com>
2022-02-06 12:59:29 +0530

    added pdf button

--- src/index.js
+++ src/index.js
@@ -3,5 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Ticket from './Ticket/ticket'
+import ReactDOMServer from "react-dom/server";
+import jsPDF from "jspdf";
+const doc = new jsPDF();
+const Foo = <Ticket />;
 
-ReactDOM.render(<App /> , document.getElementById('root'));
+export default function App1() {
+  const save = () => {
+    doc.html(ReactDOMServer.renderToStaticMarkup(Foo), {
+      callback: () => {
+        doc.save("myDocument.pdf");
+      }
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={save}>save</button>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<App1 /> , document.getElementById('root'));
